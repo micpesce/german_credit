f4523bd6b240eec035bc26a43b4bb3fc3bf47f68
micpesce <micpes@gmail.com>
2019-05-13 00:48:17 +0200

    ok

--- german_credit.R
+++ german_credit.R
@@ -3,14 +3,23 @@ library(gdata)
 library(dplyr)
 library(stringr)
 library(tidyr)
-germam_credit <- read.csv("german_credit_data.csv", header=TRUE, sep = ";")
+german_credit <- read.csv("german_credit_data.csv", header=TRUE, sep = ";")
 #exploring NA'S
-sum(is.na(germam_credit))
-sum(complete.cases(germam_credit))
+sum(is.na(german_credit))
+sum(complete.cases(german_credit))
 
-glimpse(germam_credit) #exploring data types
-germam_credit_num <- germam_credit%>% mutate_if(is.factor, as.numeric) #converting to a numeric df
-
+glimpse(german_credit) #exploring data types
+german_credit_num <- german_credit%>% mutate_if(is.factor, as.numeric) #converting to a numeric df
+glimpse(german_credit_num)
 #exploring NA'S on numerics
-sum(is.na(germam_credit_num))
-sum(complete.cases(germam_credit_num))
+sum(is.na(german_credit_num))
+sum(complete.cases(german_credit_num))
+summary(german_credit)
+credit_df <- read.csv("german.data", header=FALSE, sep = " ")
+colNames = c("checking_account", "duration", "Credit_history", "purpose", "credit_amount","savings_account","employment_since","percentage_income","personal_status","guarantors","residence","property","age","other_plans","housing","other_credits","job","house_manteinant","telephone","foreign_worker","credit")
+names(credit_df)   <-  colNames         
+glimpse(credit_df)
+
+#exploring NA'S
+sum(is.na(credit_df))
+sum(complete.cases(credit_df))
