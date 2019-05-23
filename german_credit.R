8f0919d62d48c4937fb085ff9a1db059f08ae932
micpesce <micpes@gmail.com>
2019-05-23 18:30:09 +0200

    now 2305

--- german_credit.R
+++ german_credit.R
@@ -5,6 +5,7 @@ library(stringr)
 library(tidyr)
 library(ggplot2)
 library(readtext)
+library(graphics)
 german_credit <- read.csv("german_credit_data.csv", header=TRUE, sep = ";")
 #exploring NA'S
 sum(is.na(german_credit))
@@ -26,14 +27,6 @@ glimpse(credit_original)
 sum(is.na(credit_original))
 sum(complete.cases(credit_original))
 
-library(miscset)
-unique_values <- data_frame(VARIABLE=colNames[1],UNIQUE_VAL=nunique(credit_original[,1])) #Creating first row oa tibble
-for (i in 2:length(names(credit_original))) { #calculate unique values for each variable and store them in the tibble adding rows
-  unique_values <- bind_rows(unique_values,data_frame(VARIABLE=colNames[i],UNIQUE_VAL=nunique(credit_original[,i])))
-}
-unique_values %>% knitr::kable()
-#credit_original  mutate_at()
-ggplot(credit_original,aes(x=credit_original$credit,y=credit_original$telephone))
 ####@@@@REPLACING coded values with meaningful data 
 library(tm)
 file.rename("german.doc","german.txt") #renames file to get it readable
@@ -53,6 +46,99 @@ for (i in 1:length(names(credit_clear))) { #iterates all columns
     credit_clear[,i] <- key_map$VALUE[match(credit_clear[,i], key_map$KEY)] #replace key with values
 }
 
-i <- 2
-is.factor(credit_clear[,i])
+credit_clear <- credit_clear %>% mutate(credit=ifelse(.$credit==1,"pass", "fail")) #making credit variable as categorical
+credit_num<- credit_original%>% mutate_if(is.factor, as.numeric) #converting to a numeric df
+
+
+
+
+
+#DATA ANALISYS 
+
+library(miscset)
+unique_values <- data_frame(VARIABLE=colNames[1],UNIQUE_VAL=nunique(credit_original[,1])) #Creating first row oa tibble
+for (i in 2:length(names(credit_original))) { #calculate unique values for each variable and store them in the tibble adding rows
+  unique_values <- bind_rows(unique_values,data_frame(VARIABLE=colNames[i],UNIQUE_VAL=nunique(credit_original[,i])))
+}
+unique_values %>% knitr::kable()
+#credit_original  mutate_at()
+
+#credit admission plot
+qplot(credit_clear$credit, geom="bar",
+      fill=I('gray'), col=I('black'),
+      xlab = "Credit admission" , ylab = "Count" )
+pass_amount <- length(which(credit_clear$credit=="pass"))
+fail_amount <- length(which(credit_clear$credit=="fail"))
+
+#credit amount analysis vs credit admission
+p1 <- credit_clear  %>% filter(credit=="pass") %>% ggplot(aes(x=.$credit_amount)) +
+  ggtitle(" Credit pass")+ geom_histogram(fill="green", bins=30) + geom_smooth() +
+  scale_y_continuous(limits=c(0,150))+xlab("Credit amount")+ ylab("Count")
+
+p2 <- credit_clear %>% filter(credit=="fail") %>%  ggplot(aes(x=.$credit_amount)) +
+  ggtitle(" Credit fail")+ geom_histogram(fill="red", bins=30) + geom_smooth() +
+  scale_y_continuous(limits=c(0,150))+ xlab("Credit amount")+ ylab("Count")
+
+library(gridExtra)
+grid.arrange(p1, p2, ncol = 2)
+
+#credit history analysis vs credit admission
+p1 <- credit_clear  %>% filter(credit=="pass") %>% ggplot(aes(x=.$Credit_history)) +
+  ggtitle(" Credit pass")+ geom_bar(fill="green") + 
+  scale_y_continuous(limits=c(0,400))+xlab("Credit history")+ ylab("Count") + theme(axis.text.x = element_text(angle = 90))
+
+p2 <- credit_clear %>% filter(credit=="fail") %>%  ggplot(aes(x=.$Credit_history)) +
+  ggtitle(" Credit fail")+ geom_bar(fill="red") + 
+  scale_y_continuous(limits=c(0,400))+xlab("Credit history")+ ylab("Count") + theme(axis.text.x = element_text(angle = 90))
+
+library(gridExtra)
+grid.arrange(p1, p2, ncol = 2)
+
+#Credit amount by personal status
+credit_clear  %>%  ggplot(aes(x=personal_status,y=credit_amount,fill=credit)) +
+  ggtitle(" Credit amount by personal status")+ geom_boxplot() + 
+   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ 
+#age by personal status
+credit_clear  %>%  ggplot(aes(x=personal_status,y=age,fill=credit)) +
+  ggtitle(" age by personal status")+ geom_boxplot() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#emplyment-since by credit amount
+credit_clear  %>%  ggplot(aes(x=employment_since,y=credit_amount,fill=credit)) +
+  ggtitle(" mplyment-since by credit amount")+ geom_boxplot() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#emplyment-since by credit amount based on Job
+credit_clear  %>%  ggplot(aes(x=employment_since,y=credit_amount,fill=credit)) +
+  ggtitle(" emplyment-since by credit amount")+ geom_boxplot() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  facet_grid(. ~ job)
+
+#Cheking-account VS duration based on history
+
+credit_clear %>% 
+  ggplot(aes(checking_account, duration, fill=credit)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  xlab("Cheking-account") +
+  facet_grid(. ~ Credit_history)
+
+#property VS credit_amount based on saving amount
+
+credit_clear %>% 
+  ggplot(aes(property, credit_amount, fill=credit)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  xlab("Property") +
+  facet_wrap(. ~ savings_account)
+
+#property VS credit_amount based on saving amount
+
+credit_clear %>% 
+  ggplot(aes(age, credit_amount, color=credit)) +
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  xlab("age") +
+  facet_wrap(. ~ savings_account)
 
